691fe6f0c6d1fdd5b8f6bddafaca9ea94b66fed1
devanshuK45 <devanshuwas@gmail.com>
2026-02-21 23:57:46 +0530

    dock generated

--- src/App.jsx
+++ src/App.jsx
@@ -12,5 +12,6 @@ function App() {
     </main>
   )
 }
+//yeah, this is the main app component. It imports the Dock component and renders it inside a main tag. The app also imports some styles from "app.scss".
 
 export default App
