a71280287618ede7a5e1369e98e1c364eae959a4
devanshuK45 <devanshuwas@gmail.com>
2026-02-22 00:00:55 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,5 +13,6 @@ function App() {
   )
 }
 //yeah, this is the main app component. It imports the Dock component and renders it inside a main tag. The app also imports some styles from "app.scss".
+// The useState import is not currently used in this component, but it might be used in the future for managing state. For now, the App component is pretty simple and just serves as a container for the Dock component.
 
 export default App
